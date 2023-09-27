e86304a00923e6ca5c865ede13714395f9d8f29c
David Batchelder <59281365+mini-batch@users.noreply.github.com>
2023-09-27 15:16:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import pandas as pd
 from pygame.locals import *
 import experiment_objects.Environment as e
 import frontend.SceneManager as sm
@@ -17,7 +18,7 @@ NUM_ROBOTS = 50
 UPDATE_INTERVAL = 200
 SAMPLE_CYCLE_LENGTH = 15
 SAMPLE_INTERVAL = 400
-SPEED = 0.01
+SPEED = 0.002
 COMMUNICATION_RANGE = 2
 POSITION = None#[0,0]
 SAMPLE_COLOUR = None
@@ -32,11 +33,6 @@ DEPTH = 0
 FLAGS = 0
 
 
-import experiment_objects.tests as t
-def test():
-    t.test_create_grid()
-
-
 def visualise():
     pygame.init()
     screen = pygame.display.set_mode(DISPLAY, DEPTH, FLAGS)
@@ -45,7 +41,7 @@ def visualise():
     running = True
 
     ROBOT_PARAMS = [UPDATE_INTERVAL, SAMPLE_CYCLE_LENGTH, SAMPLE_INTERVAL, SPEED, COMMUNICATION_RANGE, ENV_INTERVAL, GRID_SIZE, SAMPLE_COLOUR, DECISION_STATE, COMMITED_ESTIMATION, POSITION]
-    env = e.Environment([X_SQUARES,Y_SQUARES], COLOUR_PROB, NUM_ROBOTS, ROBOT_PARAMS, ENV_INTERVAL, EXPERIMENT_LENGTH)
+    env = e.Environment([X_SQUARES,Y_SQUARES], COLOUR_PROB, NUM_ROBOTS, ROBOT_PARAMS, ENV_INTERVAL, EXPERIMENT_LENGTH, [0.5, 10000])
     manager = sm.SceneMananger(env)
 
     while running:
@@ -67,6 +63,13 @@ import experiments.experiment1 as e1
 def experiment1():
     print(e1.run_repeat_simulation(30))
 
+import experiments.experiment2 as e2
+
+def experiment2():
+    for c_range in [1,2,3,4,5,6,7,8,9,15,25,45]:#[0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 25, 45]: 
+        results = e2.run_repeat_simulation(20, c_range)
+        df = pd.DataFrame(results, columns=["adapted", "adaptation_time", "state_history", "time_history", "color_history"])
+        df.to_csv(f"./results/exp2/sample_len/exp2_{c_range}.csv")
 
 def main():
     env = e.Environment([X_SQUARES,Y_SQUARES], COLOUR_PROB, None, 1, 15000)
@@ -78,4 +81,3 @@ if __name__=="__main__":
     #main()
     #experiment1()
     visualise()
-    #test()
