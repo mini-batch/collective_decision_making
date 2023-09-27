60bd67e944636851d8de328e97f0f6f9292e953d
David Batchelder <59281365+mini-batch@users.noreply.github.com>
2023-09-27 15:14:31 +0100

    Delete frontend directory

--- frontend/SceneManager.py
+++
@@ -1,9 +0,0 @@
-from frontend.ExperimentScene import ExperimentScene
-
-class SceneMananger():
-    def __init__(self, environment):
-        self.go_to(ExperimentScene(environment))
-
-    def go_to(self, scene):
-        self.scene = scene
-        self.scene.manager = self
